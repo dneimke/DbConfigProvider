fe344b40dc34267096dacc7a11633ab0434c58d6
Darren Neimke <darren.neimke@gmail.com>
2018-11-25 16:30:18 +0800

    Add project files.

---
+++ ConfigurationDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ConfigurationDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
